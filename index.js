842727b98018b43752823b2c4b47376944da753d
awiel <awiel@debian-laptop.scapeler.local>
2020-10-07 10:58:29 +0200

    index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+/*
+** Module: index.js
+**   OpenIod connect main module
+**
+**
+*/
+/*
+A generic connector to enable pull from source and send to target
+
+Copyright (C) 2020  André van der Wiel / Scapeler https://www.scapeler.com
+
+This work is licensed under the Creative Commons Attribution-ShareAlike 4.0 International License.
+To view a copy of this license, visit http://creativecommons.org/licenses/by-sa/4.0/ or
+send a letter to Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.along with this program.
+If not, see <https://www.gnu.org/licenses/>.
+
+*/
+
+"use strict";
